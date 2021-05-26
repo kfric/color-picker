12d289103be255ac0087e64f28728696a6738d4b
Karl Frick <karl.frick5@gmail.com>
2021-05-26 14:02:16 -0400

    Sliders added to rep Hue, saturation and Lightness

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <div>
+        <h1>---</h1>
+        <p>Hue: </p>
+        <input type="range" />
+        <p>Saturation: </p>
+        <input type="range" />
+        <p>Lightness: </p>
+        <input type="range" />
+      </div>
+    )
   }
 }
