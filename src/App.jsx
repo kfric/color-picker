0288c87353e1dc6aa61008fcad5343b78638ac80
Karl Frick <karl.frick5@gmail.com>
2021-05-26 16:15:04 -0400

    Added random buttom to setState to a random number

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,57 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    hue: Math.floor(Math.random() * 360),
+    saturation: 100,
+    lightness: 50,
+  }
+  handleRandomColor = event => {
+    this.setState({
+      hue: Math.floor(Math.random() * 360),
+      saturation: Math.floor(Math.random() * 100),
+      lightness: Math.floor(Math.random() * 100),
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h1>---</h1>
-        <p>Hue: </p>
-        <input type="range" />
-        <p>Saturation: </p>
-        <input type="range" />
-        <p>Lightness: </p>
-        <input type="range" />
-      </div>
+      <>
+        <h1>Background color changer</h1>
+        <div
+          className="changeColor"
+          style={{
+            backgroundColor: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+          }}
+        >
+          hsl(${this.state.hue}, ${this.state.saturation}%, $
+          {this.state.lightness}%)
+        </div>
+        <p>Hue: {this.state.hue}</p>
+        <input
+          max="360"
+          type="range"
+          value={this.state.hue}
+          onChange={event => this.setState({ hue: event.target.value })}
+        />
+        <p>Saturation: {this.state.saturation}</p>
+        <input
+          max="100"
+          type="range"
+          value={this.state.saturation}
+          onChange={event => this.setState({ saturation: event.target.value })}
+        />
+        <p>Lightness: {this.state.lightness}</p>
+        <input
+          max="100"
+          type="range"
+          value={this.state.lightness}
+          onChange={event => this.setState({ lightness: event.target.value })}
+        />
+        <p>
+          <button onClick={this.handleRandomColor}>RANDOM</button>
+        </p>
+      </>
     )
   }
 }
